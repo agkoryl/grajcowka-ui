bcf932de8bba8b9b8bc74af7a2fe60ff23ae3251
Agnieszka Koryl <ag.koryl@gmail.com>
2019-05-19 16:26:31 +0200

    add meeting form fix

--- src/routes/AddAMeeting/AddAMeeting.js
+++ src/routes/AddAMeeting/AddAMeeting.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MeetingIcon from '@material-ui/icons/People';
 
 import './AddAMeeting.css'
 
@@ -8,22 +9,66 @@ import { redirectToHome } from '../../services/actions';
 
 
 class AddAMeeting extends Component {
+
+
+    state = {
+
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+      }
+
+
     render() {
         return (
             <div>
                 <AppNavBar redirectToHome={() => redirectToHome(this.props.history)} />
                 <div className="meeting-container">
-                    <h2>Dodaj nowe spotkanie</h2>
+                    <div className="meeting-header">
+                        <h2>Dodaj nowe spotkanie</h2>
+                        <div className="meeting-icon"><MeetingIcon fontSize="large"></MeetingIcon></div>
+                    </div>
                     <div className="meeting-form-container">
-                        <input placeholder="nazwa spotkania" name="meetingName" type="text" className="meeting-form-input"></input>
-                        <input placeholder="nazwa gry" name="gameName" type="text" className="meeting-form-input"></input>
-                        <input placeholder="data" name="meetingName" type="text" className="meeting-form-input"></input>
-                        <input placeholder="miasto" name="meetingCity" type="text" className="meeting-form-input"></input>
-                        <input placeholder="ulica" name="meetingStreet" type="text" className="meeting-form-input"></input>
-                        <input placeholder="min liczba graczy" name="meetingMin" type="number" className="meeting-form-input"></input>
-                        <input placeholder="max liczba graczy" name="meetingMax" type="number" className="meeting-form-input"></input>
-                       <button type="button" className="meeting-add-button">
-                       Dodaj
+                        <div className="meeting-input-container">
+                            <div className="meeting-input-title">
+                                <p>
+                                    Nazwa spotkania
+                            </p></div>
+                            <input name="meetingName" type="text" className="meeting-form-input" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="meeting-input-container">
+                            <div className="meeting-input-title"><p>
+                                Nazwa gry
+                            </p></div>
+                            <input name="gameName" type="text" className="meeting-form-input" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="meeting-input-container">
+                            <div className="meeting-input-title"><p>
+                                Data spotkania
+                            </p></div>
+                            <input name="meetingDate" type="date" className="meeting-form-input" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="meeting-input-container">
+                            <div className="meeting-input-title"><p>
+                                Miasto
+                            </p></div>
+                            <input name="meetingCity" type="text" className="meeting-form-input" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="meeting-input-container">
+                            <div className="meeting-input-title"><p>
+                                Ulica
+                            </p></div>
+                            <input name="meetingStreet" type="text" className="meeting-form-input" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="meeting-input-container">
+                            <div className="meeting-input-title"><p>
+                                Max liczba graczy
+                            </p></div>
+                            <input name="meetingMax" placeholder="0" type="number" className="meeting-form-input" onChange={this.handleChange}></input>
+                        </div>
+                        <button type="button" className="meeting-add-button">
+                            DODAJ
                        </button>
                     </div>
                 </div>
